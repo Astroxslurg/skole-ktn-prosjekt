55642fab8d2a4aeaaf9609c98af29be46a171856
Kristian Trandem <kkrorkk@gmail.com>
2017-03-16 17:20:44 +0100

    make MessageParser compile

--- skeleton/Client/MessageParser.py
+++ skeleton/Client/MessageParser.py
@@ -1,24 +1,27 @@
-
+import json
 
 class MessageParser():
     def __init__(self):
-
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
-	    # More key:values pairs are needed	
+	    # More key:values pairs are needed
         }
 
     def parse(self, payload):
-        payload = # decode the JSON object
+        payload = json.loads(payload)
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
+            print("Error: Invalid response type." +
+                  "There is no parser for response type called" +
+                  payload['response'])
 
     def parse_error(self, payload):
-    
+        print(payload)
+
     def parse_info(self, payload):
-    
-    # Include more methods for handling the different responses... 
+        print(payload)
+
+    # Include more methods for handling the different responses...
