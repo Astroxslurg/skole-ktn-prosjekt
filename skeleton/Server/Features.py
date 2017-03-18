98293d505735bf789aa3c01415a8f9d4fa8b8ecc
Kristian Trandem <kkrorkk@gmail.com>
2017-03-18 16:22:31 +0100

    cleanup in Features.py

--- skeleton/Server/Features.py
+++ skeleton/Server/Features.py
@@ -6,9 +6,9 @@ General Helpers
 
 
 def isRequestValid(request, expectedType):
-	requestIsAsExpect = request["request"] == expectedType
-	if requestIsAsExpect is False:
-		raise ValueError("request was not: " + expectedType)
+    requestIsAsExpect = request["request"] == expectedType
+    if requestIsAsExpect is False:
+        raise ValueError("request was not: " + expectedType)
 
 
 """
@@ -17,85 +17,87 @@ Login helpers
 
 
 def loginError():
-	data = {
-		'response': "error",
-		'content': "That name is taken",
-		'sender': "server"
-	}
+    data = {
+        'response': "error",
+        'content': "That name is taken",
+        'sender': "server"
+    }
 
-	return ResponseGenerator(data).jsonPayload()
+    return ResponseGenerator(data).jsonPayload()
+
 
 def addUsername(username, state):
-	pass
+    pass
+
 
 def isUsernameTaken(username, state):
-	usernames = state.getUsernames()
-	print(usernames)
-
+    usernames = state.getUsernames()
+    print(usernames)
 
 
 def loginSuccessful():
-	data = {
-		'response': "info",
-		'content': "Login successful",
-		'sender': "server"
-	}
-	return ResponseGenerator(data).jsonPayload()
+    data = {
+        'response': "info",
+        'content': "Login successful",
+        'sender': "server"
+    }
+    return ResponseGenerator(data).jsonPayload()
 
 
 class Features():
-	def __init__(self, state):
-		self.state = state
+    def __init__(self, state, currentConnection):
+        self.state = state
+        self.currentConnection = currentConnection
 
-	def login(self, request):
-		isRequestValid(request, "login")
-		username = request["content"]
+    def login(self, request):
+        isRequestValid(request, "login")
+        username = request["content"]
 
-		if isUsernameTaken(username, self.state):
-			return loginError()
+        if isUsernameTaken(username, self.state):
+            return loginError()
 
-		addUsername(username, self.state)
+        addUsername(username, self.state)
 
-		return loginSuccessful()
+        return loginSuccessful()
 
-	def msg(self, request):
-		isRequestValid(request, 'msg')
-		data = {
-			'sender': '<USER>',
-			'response': 'message',
-			'content': request["content"],
-		}
-		json = ResponseGenerator(data).jsonPayload()
+    def msg(self, request):
+        isRequestValid(request, 'msg')
+        data = {
+            'sender': '<USER>',
+            'response': 'message',
+            'content': request["content"],
+        }
+        json = ResponseGenerator(data).jsonPayload()
 
-		return json
+        return json
 
-	def help(self, request):
-		data = {
-			'response': "info",
-			'content': "Help is here!",
-			'sender': "server"
-		}
-		json = ResponseGenerator(data).jsonPayload()
+    def help(self, request):
+        data = {
+            'response': "info",
+            'content': "Help is here!",
+            'sender': "server"
+        }
+        json = ResponseGenerator(data).jsonPayload()
 
-		return json
+        return json
 
-	def	names(self, request):
-		data = {
-			'response': 'info',
-			'content': state['usernames'],
-			'sender': 'server'
-		}
-		json = ResponseGenerator(data).jsonPayload()
-		return json
+    def names(self, request):
+        data = {
+            'response': 'info',
+            'content': self.state['usernames'],
+            'sender': 'server'
+        }
+        json = ResponseGenerator(data).jsonPayload()
+        return json
 
-	def logout(self, request):
-		print("hei")
+    def logout(self, request):
+        print("hei")
 
-	def	noSuchMethod():
+    def noSuchMethod():
 
-			data = {
-				sender: "server",
-				response: "Error",
-				content: "There is no such command"
-			}
-			return ResponseGenerator(data)
+        data = {
+            'sender': "server",
+            'response': "Error",
+            'content': "There is no such command"
+        }
+        return ResponseGenerator(data)
