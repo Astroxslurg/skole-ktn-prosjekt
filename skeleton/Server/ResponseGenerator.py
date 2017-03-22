982793932ab5c5e1a6bc4a943678201b2f65adfe
Kristian Trandem <kkrorkk@gmail.com>
2017-03-22 20:48:51 +0100

    implement timestamp

--- skeleton/Server/ResponseGenerator.py
+++ skeleton/Server/ResponseGenerator.py
@@ -1,5 +1,5 @@
 import json
-
+import time
 
 class ResponseGenerator():
 
@@ -11,7 +11,7 @@ class ResponseGenerator():
         self.timestamp = self.getTimestamp
 
     def getTimestamp(self):
-        return "Partytime!"
+        return time.time()
 
     def payload(self):
         return {
